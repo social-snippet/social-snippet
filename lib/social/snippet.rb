974637e3169089ed9cb0346cfe12f37229bdd6f2
Hiroyuki Sano <sh19910711@gmail.com>
2014-08-06 16:11:21 +0900

    bundle gem

---
+++ lib/social/snippet.rb
@@ -0,0 +1,7 @@
+require "social/snippet/version"
+
+module Social
+  module Snippet
+    # Your code goes here...
+  end
+end
---
+++ lib/social/snippet/version.rb
@@ -0,0 +1,5 @@
+module Social
+  module Snippet
+    VERSION = "0.0.1"
+  end
+end
