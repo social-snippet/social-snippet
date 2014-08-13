6d346b02403771409d444ea139a593a99773ebe5
Hiroyuki Sano <sh19910711@gmail.com>
2014-08-13 16:22:05 +0900

    tag: add constructor

--- lib/social_snippet/tag.rb
+++ lib/social_snippet/tag.rb
@@ -1,4 +1,16 @@
 module SocialSnippet
   class Tag
+    # Create instance
+    #
+    # @param s [String] tag line text
+    def initialize(s)
+      if is_tag_line(s)
+        @path   = get_path(s)
+        @repo   = get_repo(s)
+        @prefix = get_prefix(s)
+        @suffix = get_suffix(s)
+        @spaces = get_spaces(s)
+      end
+    end
   end
 end
