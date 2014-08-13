982c710fcce9a76fb786147dc3d23553bfb57c74
Hiroyuki Sano <sh19910711@gmail.com>
2014-08-13 16:17:46 +0900

    tag_parser: add tag_parser.rb

---
+++ lib/social_snippet/tag_parser.rb
@@ -0,0 +1,10 @@
+module SocialSnippet
+  class TagParser
+    # Find `@snip` tags from text
+    #
+    # @param text [String] parsed text
+    # @return [Array] found `@snip` tags
+    def find_snip_tags(text)
+    end
+  end
+end
