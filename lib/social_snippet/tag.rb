dd3f84863327d6cae97a902a3e01fbac8b7c0637
Hiroyuki Sano <sh19910711@gmail.com>
2014-08-18 14:41:34 +0900

    tag: add is_snip_tag_line() / is_snippet_tag_line()
    * to find @snip/@snippet tag

--- lib/social_snippet/tag.rb
+++ lib/social_snippet/tag.rb
@@ -13,6 +13,15 @@ module SocialSnippet
 
     class << self
 
+      # Check given line to match @snip tag
+      def is_snip_tag_line(s)
+        return /@snip\s*<.*?>/.match(s)
+      end
+
+      # Check given line to match @snippet tag
+      def is_snippet_tag_line(s)
+        return /@snippet\s*<.*?>/.match(s)
+      end
 
     end
   end
--- test/tag_test.rb
+++ test/tag_test.rb
@@ -4,6 +4,92 @@ module SocialSnippet
 
   describe Tag do
 
+    describe "#is_snip_tag_line()" do
+
+      context "valid cases" do
+
+        context "relative path without `./`" do
+          it { expect(Tag.is_snip_tag_line("// @snip <path/to/file.cpp>")).to be_truthy }
+          it { expect(Tag.is_snip_tag_line("/* @snip <path/to/file.c> */")).to be_truthy }
+          it { expect(Tag.is_snip_tag_line("# @snip <path/to/file.py>")).to be_truthy }
+        end
+
+        context "relative path start with `./`" do
+          it { expect(Tag.is_snip_tag_line("// @snip <./path/to/file.cpp>")).to be_truthy }
+          it { expect(Tag.is_snip_tag_line("/* @snip <./path/to/file.c> */")).to be_truthy }
+          it { expect(Tag.is_snip_tag_line("# @snip <./path/to/file.py>")).to be_truthy }
+        end
+
+        context "with repo without `/`" do
+          it { expect(Tag.is_snip_tag_line("// @snip <repo:path/to/file.cpp>")).to be_truthy }
+          it { expect(Tag.is_snip_tag_line("/* @snip <repo:path/to/file.c> */")).to be_truthy }
+          it { expect(Tag.is_snip_tag_line("# @snip <repo:path/to/file.py>")).to be_truthy }
+        end
+
+        context "with repo and start with `/`" do
+          it { expect(Tag.is_snip_tag_line("// @snip <repo:/path/to/file.cpp>")).to be_truthy }
+          it { expect(Tag.is_snip_tag_line("/* @snip <repo:/path/to/file.c> */")).to be_truthy }
+          it { expect(Tag.is_snip_tag_line("# @snip <repo:/path/to/file.py>")).to be_truthy }
+        end
+
+        # TODO: add `@snip <{repo}#{version}:{path}>`
+
+      end # valid cases
+
+      context "invalid cases" do
+        it { expect(Tag.is_snip_tag_line("// @snip2 <path/to/file.cpp>")).to be_falsey }
+        it { expect(Tag.is_snip_tag_line("// @sni <path/to/file.cpp>")).to be_falsey }
+        it { expect(Tag.is_snip_tag_line("// @snip <path/to/file.cpp")).to be_falsey }
+        it { expect(Tag.is_snip_tag_line("// @snip path/to/file.cpp>")).to be_falsey }
+        it { expect(Tag.is_snip_tag_line("/* @ snip <path/to/file.c> */")).to be_falsey }
+        it { expect(Tag.is_snip_tag_line("# @snp <path/to/file.py>")).to be_falsey }
+      end # invalid cases
+
+    end # is_snip_tag_line
+
+    describe "#is_snippet_tag_line()" do
+
+      context "valid cases" do
+
+        context "relative path without `./`" do
+          it { expect(Tag.is_snippet_tag_line("// @snippet <path/to/file.cpp>")).to be_truthy }
+          it { expect(Tag.is_snippet_tag_line("/* @snippet <path/to/file.c> */")).to be_truthy }
+          it { expect(Tag.is_snippet_tag_line("# @snippet <path/to/file.py>")).to be_truthy }
+        end
+
+        context "relative path start with `./`" do
+          it { expect(Tag.is_snippet_tag_line("// @snippet <./path/to/file.cpp>")).to be_truthy }
+          it { expect(Tag.is_snippet_tag_line("/* @snippet <./path/to/file.c> */")).to be_truthy }
+          it { expect(Tag.is_snippet_tag_line("# @snippet <./path/to/file.py>")).to be_truthy }
+        end
+
+        context "with repo" do
+          it { expect(Tag.is_snippet_tag_line("// @snippet <repo:path/to/file.cpp>")).to be_truthy }
+          it { expect(Tag.is_snippet_tag_line("/* @snippet <repo:path/to/file.c> */")).to be_truthy }
+          it { expect(Tag.is_snippet_tag_line("# @snippet <repo:path/to/file.py>")).to be_truthy }
+        end
+
+        context "with repo start with `/`" do
+          it { expect(Tag.is_snippet_tag_line("// @snippet <repo:/path/to/file.cpp>")).to be_truthy }
+          it { expect(Tag.is_snippet_tag_line("/* @snippet <repo:/path/to/file.c> */")).to be_truthy }
+          it { expect(Tag.is_snippet_tag_line("# @snippet <repo:/path/to/file.py>")).to be_truthy }
+        end
+
+        # TODO: add `@snippet <{repo}#{version}:{path}>`
+
+      end # valid cases
+
+      context "invalid cases" do
+        it { expect(Tag.is_snippet_tag_line("// @snippet2 <path/to/file.cpp>")).to be_falsey }
+        it { expect(Tag.is_snippet_tag_line("// @snippe <path/to/file.cpp>")).to be_falsey }
+        it { expect(Tag.is_snippet_tag_line("// @snippet <path/to/file.cpp")).to be_falsey }
+        it { expect(Tag.is_snippet_tag_line("// @snippet path/to/file.cpp>")).to be_falsey }
+        it { expect(Tag.is_snippet_tag_line("/* @ snippet <path/to/file.c> */")).to be_falsey }
+        it { expect(Tag.is_snippet_tag_line("# @snppet <path/to/file.py>")).to be_falsey }
+      end
+
+    end # is_snippet_tag_line
+
   end # Tag
 
 end # SocialSnippet
