5a64f586ec95753b8f6d25c6be66a97b7e7a3580
Hiroyuki Sano <sh19910711@gmail.com>
2014-08-18 14:13:00 +0900

    tag: small changes
    * rewrite initialize()
      * to use class method
    * remove is_tag_line()
      * to be is_snip_tag_line() / is_snippet_tag_line()

--- lib/social_snippet/tag.rb
+++ lib/social_snippet/tag.rb
@@ -4,21 +4,15 @@ module SocialSnippet
     #
     # @param s [String] tag line text
     def initialize(s)
-      if is_tag_line(s)
-        @path   = get_path(s)
-        @repo   = get_repo(s)
-        @prefix = get_prefix(s)
-        @suffix = get_suffix(s)
-        @spaces = get_spaces(s)
-      end
+      @path   = Tag.get_path(s)
+      @repo   = Tag.get_repo(s)
+      @prefix = Tag.get_prefix(s)
+      @suffix = Tag.get_suffix(s)
+      @spaces = Tag.get_spaces(s)
     end
 
     class << self
 
-      # Check line text contains tag
-      def is_tag_line(s)
-        /(@snippet|@snip)\s*<.*?>/.match s
-      end
 
     end
   end
