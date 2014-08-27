3840794c1e62d1239917552af65f2b067d18a911
Hiroyuki Sano <sh19910711@gmail.com>
2014-08-27 16:57:11 +0900

    base_repository: add load_snippet_json()

--- lib/social_snippet/repository/base_repository.rb
+++ lib/social_snippet/repository/base_repository.rb
@@ -15,6 +15,15 @@ module SocialSnippet
         @cache_path = nil
       end
 
+      # Load snippet.json file
+      def load_snippet_json()
+        text = File.read("#{@path}/snippet.json")
+        snippet_json = JSON.parse(text)
+        @name = snippet_json["name"]
+        @desc = snippet_json["desc"]
+        @main = snippet_json["main"] || ""
+      end
+
       # Get current ref's commit id
       def get_commit_id()
         raise "not implement get_commit_id()"
