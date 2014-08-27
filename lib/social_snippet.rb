165c7eb729ae3af4907fd49d456abdb3b36b8587
Hiroyuki Sano <sh19910711@gmail.com>
2014-08-27 16:30:27 +0900

    config: add Config class
    * add constructor
    * add load_from_environment_variables()
    * add load_default_value()

--- lib/social_snippet.rb
+++ lib/social_snippet.rb
@@ -1,6 +1,7 @@
 require_relative "social_snippet/version"
 require_relative "social_snippet/tag"
 require_relative "social_snippet/tag_parser"
+require_relative "social_snippet/config"
 require_relative "social_snippet/context"
 
 module SocialSnippet
