e76b63eb319a4689978d02da17c68bf8ea698e80
Hiroyuki Sano <sh19910711@gmail.com>
2014-09-08 09:40:07 +0900

    tag_parser: change instance methods to class methods

--- lib/social_snippet/tag_parser.rb
+++ lib/social_snippet/tag_parser.rb
@@ -1,41 +1,61 @@
 module SocialSnippet
   class TagParser
-    # Find `@snip` tags from text
-    #
-    # @param s [String] parsed text
-    # @return [Array] found `@snip` tags with line_no
-    def find_snip_tags(s)
-      found_lines = []
-      s.split("\n").each.with_index do |line, i|
-        if Tag.is_snip_tag_line(line)
-          found_lines.push(
-            {
-              :line_no => i,
-              :tag => Tag.new(line),
-            }
-          )
-        end
-      end
-      return found_lines
-    end
+    class << self
 
-    # Find `@snippet` tags from text
-    #
-    # @param s [String] parsed text
-    # @return [Array] found `@snippet` tags with line_no
-    def find_snippet_tags(s)
-      found_lines = []
-      s.split("\n").each.with_index do |line, i|
-        if Tag.is_snippet_tag_line(line)
-          found_lines.push(
-            {
-              :line_no => i,
-              :tag => Tag.new(line),
-            }
-          )
+      # Find `@snip` tags from text
+      #
+      # @param s [String or Array] parsed text
+      # @return [Array] found `@snip` tags with line_no
+      def find_snip_tags(s)
+        found_lines = []
+
+        lines = get_lines(s)
+
+        lines.each.with_index do |line, i|
+          if Tag.is_snip_tag_line(line)
+            found_lines.push(
+              {
+                :line_no => i,
+                :tag => Tag.new(line),
+              }
+            )
+          end
+        end
+
+        return found_lines
+      end
+
+      # Find `@snippet` tags from text
+      #
+      # @param s [String or Array] parsed text
+      # @return [Array] found `@snippet` tags with line_no
+      def find_snippet_tags(s)
+        found_lines = []
+
+        lines = get_lines(s)
+
+        lines.each.with_index do |line, i|
+          if Tag.is_snippet_tag_line(line)
+            found_lines.push(
+              {
+                :line_no => i,
+                :tag => Tag.new(line),
+              }
+            )
+          end
+        end
+
+        return found_lines
+      end
+
+      def get_lines(s)
+        if s.is_a?(String)
+          return s.split("\n")
+        elsif s.is_a?(Array)
+          return s
         end
       end
-      return found_lines
+
     end
 
   end
