e31d08976e8327b5b9bee3465e05b4f360a715cc
Hiroyuki Sano <sh19910711@gmail.com>
2014-09-08 09:36:56 +0900

    repository: support version refs

--- lib/social_snippet/repository/base_repository.rb
+++ lib/social_snippet/repository/base_repository.rb
@@ -10,17 +10,16 @@ module SocialSnippet
       attr_reader :main
 
       # Constructor
+      #
+      # @param path [String] The path of repository
       def initialize(path)
         @path = path
         @cache_path = nil
       end
 
-      # Check repository is cached
-      def is_cached?
-        return @cache_path.nil? === false
-      end
-
       # Create repository cache
+      #
+      # @param cache_path [String] The path of cache dir
       def create_cache(cache_path)
         cache_to = get_commit_id[0..7]
         @cache_path = "#{cache_path}/#{@name}/#{cache_to}"
@@ -37,21 +36,74 @@ module SocialSnippet
         @main = snippet_json["main"] || ""
       end
 
+      # Get latest version
+      #
+      # @param pattern [String] The pattern of version
+      # @return [String] The version
+      def get_latest_version(pattern = "")
+        pattern = "" if pattern.nil?
+        versions = get_versions.select {|ref| Version.is_matched_version_pattern(pattern, ref)}
+        return VersionSorter.rsort(versions).first
+      end
+
+      # Get all versions
+      #
+      # @return [Array<String>] All versions of repository
+      def get_versions
+        return get_refs.select {|ref| Version.is_version(ref) }
+      end
+
+      # Get all refs
+      #
+      # @return [Array<String>] All references of repository
+      def get_refs
+        raise "not implement get_refs"
+      end
+
+      # Get short current ref's commit id
+      #
+      # @return [String]
+      def get_short_commit_id
+        return get_commit_id[0..7]
+      end
+
       # Get current ref's commit id
+      #
+      # @return [String]
       def get_commit_id
         raise "not implement get_commit_id"
       end
 
+      # Get real path
+      #
+      # @param target_path [String] The real path of repo's file or directory
+      def get_real_path(target_path)
+        if is_cached?
+          return "#{@cache_path}/#{@main}/#{target_path}"
+        else
+          return "#{@path}/#{@main}/#{target_path}"
+        end
+      end
+
       # Checkout to ref
+      #
+      # @param ref [String] The reference of repository
       def checkout(ref)
         raise "not implement checkout"
       end
 
-      class << self
-        # Check given text is version string
-        def is_version(s)
-          return /^([0]|[1-9][0-9]*)\.([0]|[1-9][0-9]*)\.([0]|[1-9][0-9]*)$/ === s
-        end
+      # Check repository has version ref
+      #
+      # @return [Boolean]
+      def has_versions?
+        return get_versions.empty? === false
+      end
+
+      # Check repository is cached
+      #
+      # @return [Boolean]
+      def is_cached?
+        return @cache_path.nil? === false
       end
     end
   end
