93cfe986233a48c5e9581cbcae6557cd132016b5
Hiroyuki Sano <sh19910711@gmail.com>
2014-09-08 09:42:04 +0900

    social_snippet: use external classes

--- lib/social_snippet.rb
+++ lib/social_snippet.rb
@@ -3,80 +3,48 @@ require_relative "social_snippet/tag"
 require_relative "social_snippet/tag_parser"
 require_relative "social_snippet/config"
 require_relative "social_snippet/repository"
+require_relative "social_snippet/repository_manager"
 require_relative "social_snippet/context"
+require_relative "social_snippet/snippet"
+require_relative "social_snippet/inserter"
+require_relative "social_snippet/snippet_finder"
+
+require "version_sorter"
+require "tsort"
+
+# Extend Hash tsortable
+class Hash
+  include TSort
+  alias tsort_each_node each_key
+  def tsort_each_child(node, &block)
+    fetch(node).each(&block)
+  end
+end
 
 module SocialSnippet
+
   class SocialSnippet
+    attr_reader :repo_manager
+
     # Constructor
     def initialize
-      @config = Config.new
+      @config = Config.new.freeze
       init_repo
     end
 
     # Initialize for repository
     def init_repo
-      # repositories
-      @repo_paths = []
-      @repo_paths.push "#{@config.home}/repo"
-      @repo_paths.each {|path| FileUtils.mkdir_p path }
-
-      # repository cache
-      @repo_cache_path = "#{@config.home}/repo_cache"
-      FileUtils.mkdir_p @repo_cache_path
+      @repo_manager = RepositoryManager.new(@config)
     end
 
-    # Resolve snippet path from tag
-    def resolve_snippet_path(context, tag)
-      if tag.has_repo?
-        context.move tag.path, tag.repo
-        repo = find_repository(tag.repo)
-        if repo.is_cached?
-          return "#{repo.cache_path}/#{repo.main}/#{tag.path}"
-        else
-          return "#{repo.path}/#{repo.main}/#{tag.path}"
-        end
-      end
-
-      context.move tag.path
-      return context.path
+    # Insert snippets to given text
+    #
+    # @param src [String] The text of source code
+    def insert_snippet(src)
+      searcher = SnippetFinder::SnippetFinderWithInsert.new(repo_manager)
+      return searcher.insert(src)
     end
 
-    # Find repository by repo name
-    def find_repository(name, ref = nil)
-      @repo_paths.each do |repo_path|
-        path = "#{repo_path}/#{name}"
-        if Dir.exists?(path)
-          return create_repository_instance(path, ref)
-        end
-      end
-
-      return nil
-    end
-
-    # Create suitable Repository class instance
-    def create_repository_instance(path, ref = nil)
-      if is_git_dir(path)
-        repo = Repository::GitRepository.new(path)
-        repo.checkout(ref)
-        repo.load_snippet_json
-        repo.create_cache(@repo_cache_path)
-        return repo
-      end
-
-      return nil
-    end
-
-    # Alias for SocialSnippet.is_git_dir
-    def is_git_dir(path)
-      return SocialSnippet.is_git_dir(path)
-    end
-
-    class << self
-      # Check given path is git repository
-      def is_git_dir(path)
-        return Dir.exists?("#{path}/.git")
-      end
-    end
   end
 end
 
