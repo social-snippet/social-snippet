c927173bfe449ae0f6b224ed1da1cbf32dbdd321
Hiroyuki Sano <sh19910711@gmail.com>
2014-09-12 10:57:17 +0900

    social_snippet: fix snip self indirectly case

--- lib/social_snippet/snippet_finder/snippet_finder_with_dep.rb
+++ lib/social_snippet/snippet_finder/snippet_finder_with_dep.rb
@@ -17,7 +17,14 @@ module SocialSnippet
       # @param context_from [SocialSnippet::Context] The context of previous code
       # @param tag_from [SocialSnippet::Tag] The previous tag
       def find(src, context_from, tag_from)
-        return find_func src, context_from, tag_from
+        found_tags = find_func src, context_from, tag_from
+        found_tags.each do |tag_info|
+          # remove self from deps graph
+          tag = tag_info[:tag]
+          @dep_to[tag.to_path] ||= Set.new
+          @dep_to[tag.to_path].delete tag_from.to_path
+        end
+        found_tags
       end
 
       private
