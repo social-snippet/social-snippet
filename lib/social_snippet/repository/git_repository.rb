d1b730a432500375baf9eff887cdce3c181234e8
Hiroyuki Sano <sh19910711@gmail.com>
2014-09-29 16:09:45 +0900

    git_repository: add download() to clone repo

--- lib/social_snippet/repository/git_repository.rb
+++ lib/social_snippet/repository/git_repository.rb
@@ -1,6 +1,15 @@
 module SocialSnippet
   module Repository
     class GitRepository < BaseRepository
+
+      class << self
+
+        # @return path
+        def download(uri)
+          raise "not implement download_git_repo"
+        end
+
+      end
     end
   end
 end
