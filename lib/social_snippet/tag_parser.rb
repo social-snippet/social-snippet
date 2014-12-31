6f5266cb44d19d131ed788759cf9467c5f1c9de3
Hiroyuki Sano <sh19910711@gmail.com>
2014-12-31 18:14:08 +0900

    tag_parser: clean up code

--- lib/social_snippet/tag_parser.rb
+++ lib/social_snippet/tag_parser.rb
@@ -1,62 +1,61 @@
-module SocialSnippet
-  class TagParser
-    class << self
+class SocialSnippet::TagParser
 
-      # Find `@snip` tags from text
-      #
-      # @param s [String or Array] parsed text
-      # @return [Array] found `@snip` tags with line_no
-      def find_snip_tags(s)
-        found_lines = []
+  class << self
 
-        lines = get_lines(s)
+    # Find `@snip` tags from text
+    #
+    # @param s [String or Array] parsed text
+    # @return [Array] found `@snip` tags with line_no
+    def find_snip_tags(s)
+      found_lines = []
 
-        lines.each.with_index do |line, i|
-          if Tag.is_snip_tag_line(line)
-            found_lines.push(
-              {
-                :line_no => i,
-                :tag => Tag.new(line),
-              }
-            )
-          end
-        end
+      lines = get_lines(s)
 
-        return found_lines
-      end
-
-      # Find `@snippet` tags from text
-      #
-      # @param s [String or Array] parsed text
-      # @return [Array] found `@snippet` tags with line_no
-      def find_snippet_tags(s)
-        found_lines = []
-
-        lines = get_lines(s)
-
-        lines.each.with_index do |line, i|
-          if Tag.is_snippet_tag_line(line)
-            found_lines.push(
-              {
-                :line_no => i,
-                :tag => Tag.new(line),
-              }
-            )
-          end
-        end
-
-        return found_lines
-      end
-
-      def get_lines(s)
-        if s.is_a?(String)
-          return s.split("\n")
-        elsif s.is_a?(Array)
-          return s
+      lines.each.with_index do |line, i|
+        if ::SocialSnippet::Tag.is_snip_tag_line(line)
+          found_lines.push(
+            {
+              :line_no => i,
+              :tag => ::SocialSnippet::Tag.new(line),
+            }
+          )
         end
       end
 
+      return found_lines
+    end
+
+    # Find `@snippet` tags from text
+    #
+    # @param s [String or Array] parsed text
+    # @return [Array] found `@snippet` tags with line_no
+    def find_snippet_tags(s)
+      found_lines = []
+
+      lines = get_lines(s)
+
+      lines.each.with_index do |line, i|
+        if ::SocialSnippet::Tag.is_snippet_tag_line(line)
+          found_lines.push(
+            {
+              :line_no => i,
+              :tag => ::SocialSnippet::Tag.new(line),
+            }
+          )
+        end
+      end
+
+      return found_lines
+    end
+
+    def get_lines(s)
+      if s.is_a?(String)
+        return s.split("\n")
+      elsif s.is_a?(Array)
+        return s
+      end
     end
 
   end
+
 end
