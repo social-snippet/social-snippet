4aea837c18f705a71cd94c80a09a92fa12e589a8
Hiroyuki Sano <sh19910711@gmail.com>
2014-12-31 18:07:40 +0900

    sspm-search: use Api class

--- lib/social_snippet/command_line/sspm/sub_commands/search_command.rb
+++ lib/social_snippet/command_line/sspm/sub_commands/search_command.rb
@@ -1,96 +1,30 @@
-module SocialSnippet
+module SocialSnippet::CommandLine
 
-  module CommandLine
+  class SSpm::SubCommands::SearchCommand < Command
 
-    module Sspm
+    def usage
+      <<EOF
+Usage: sspm search [options] [--] <keyword>
+EOF
+    end
 
-      module SubCommands
+    def desc
+      "Search for repository by keyword"
+    end
 
-        class SearchCommand < Command
+    def define_options
+      define_option :name, :type => :flag, :short => true, :default => true
+      define_option :desc, :type => :flag, :short => true, :default => true
+      define_option :url, :type => :flag, :short => true, :default => false
+      define_option :installed, :type => :flag, :short => true, :default => true
+    end
 
-          attr_reader :query
-          attr_reader :client
-
-          def initialize(new_args)
-            super
-
-            @query = args.find {|arg| is_not_line_option? arg }
-
-            @client = ::SocialSnippet::RegistryClient.new(
-              SSPM_API_HOST,
-              SSPM_API_VERSION,
-              SSPM_API_PROTOCOL,
-            )
-          end
-
-          def define_options
-            # show name
-            opt_parser.on "-n", "--[no-]name" do |v|
-              options[:show_name] = v
-            end
-
-            # show desc
-            opt_parser.on "-d", "--[no-]desc" do |v|
-              options[:show_desc] = v
-            end
-
-            # show url
-            opt_parser.on "-u", "--[no-]url" do |v|
-              options[:show_url] = v
-            end
-          end
-
-          def set_default_options
-            options[:show_name] = true if options[:show_name].nil?
-            options[:show_desc] = true if options[:show_desc].nil?
-            options[:show_url]  = false if options[:show_url].nil?
-          end
-
-          def run
-            client.get_repositories(query).each do |repo|
-              puts output_format % output_list(repo)
-            end
-          end
-
-          private
-
-          def output_format
-            f = ""
-            if options[:show_name]
-              f += "%s"
-            end
-            if options[:show_desc]
-              if options[:show_name]
-                f += ": "
-              end
-              f += "%s"
-            end
-            if options[:show_url]
-              if options[:show_name] && options[:show_desc]
-                f += " (%s)"
-              elsif options[:show_name]
-                f += ": %s"
-              elsif options[:show_desc]
-                f += " (%s)"
-              else
-                f += "%s"
-              end
-            end
-            return f
-          end
-
-          def output_list(repo)
-            list = []
-            list.push repo["name"] if options[:show_name]
-            list.push repo["desc"] if options[:show_desc]
-            list.push repo["url"] if options[:show_url]
-            return list
-          end
-
-        end
-
+    def run
+      if has_next_token?
+        social_snippet.api.search_repositories next_token, options
+      else
+        help
       end
-
     end
 
   end
