914b5ef890434da0e95060894e2a24fa1ce49791
Hiroyuki Sano <sh19910711@gmail.com>
2014-12-31 18:03:55 +0900

    sspm: add sspm-complete command

---
+++ lib/social_snippet/command_line/sspm/sub_commands/complete_command.rb
@@ -0,0 +1,28 @@
+module SocialSnippet::CommandLine
+
+  class SSpm::SubCommands::CompleteCommand < Command
+
+    def usage
+      <<EOF
+Usage: sspm complete [options] [--] <keyword>
+EOF
+    end
+
+    def desc
+      "Complete snippet paths (for editor plugins)"
+    end
+
+    def define_options
+    end
+
+    def run
+      if has_next_token?
+        social_snippet.api.cli_complete_snippet_path next_token
+      else
+        help
+      end
+    end
+
+  end
+
+end
