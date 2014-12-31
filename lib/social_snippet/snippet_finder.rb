103ba752e485c9d99a6842e5dced4ede0a5bc58a
Hiroyuki Sano <sh19910711@gmail.com>
2014-12-31 18:15:16 +0900

    lib: delete old classes and modules

--- lib/social_snippet/snippet_finder.rb
+++
@@ -1,8 +0,0 @@
-require_relative "snippet_finder/snippet_finder_base"
-require_relative "snippet_finder/snippet_finder_with_insert"
-require_relative "snippet_finder/snippet_finder_with_dep"
-
-module SocialSnippet
-  module SnippetFinder
-  end
-end
