d7bdc906b0eab3343d98baca766b4f92f5c3ce6d
Hiroyuki Sano <sh19910711@gmail.com>
2015-01-14 12:34:51 +0900

    test: add tests for sspm-config command

---
+++ spec/lib/command_line/sspm_config_spec.rb
@@ -0,0 +1,48 @@
+require "spec_helper"
+
+module SocialSnippet::CommandLine
+
+  describe SSpm::SubCommands::ConfigCommand, :current => true do
+
+    describe "$ sspm config key" do
+
+      let(:sspm_config_command) { SSpm::SubCommands::ConfigCommand.new ["key"] }
+
+      it { expect(fake_social_snippet.api).to receive(:config_get).with("key").once }
+
+      after do
+        sspm_config_command.init
+        sspm_config_command.run
+      end
+
+    end
+
+    describe "$ sspm config key value" do
+
+      let(:sspm_config_command) { SSpm::SubCommands::ConfigCommand.new ["key", "value"] }
+
+      it { expect(fake_social_snippet.api).to receive(:config_set).with("key", "value").once }
+
+      after do
+        sspm_config_command.init
+        sspm_config_command.run
+      end
+
+    end
+
+    describe "$ sspm config key=value" do
+
+      let(:sspm_config_command) { SSpm::SubCommands::ConfigCommand.new ["key=value"] }
+
+      it { expect(fake_social_snippet.api).to receive(:config_set).with("key", "value").once }
+
+      after do
+        sspm_config_command.init
+        sspm_config_command.run
+      end
+
+    end
+
+  end # SSpm::MainCommand
+
+end # SocialSnippet::CommandLine
