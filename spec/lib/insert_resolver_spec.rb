498d4aee0ba5d5a9f8c18d96c9ff37efded0424d
Hiroyuki Sano <sh19910711@gmail.com>
2015-01-20 15:51:36 +0900

    insert_resolver_spec: add tests for margin options

--- spec/lib/insert_resolver_spec.rb
+++ spec/lib/insert_resolver_spec.rb
@@ -13,9 +13,11 @@ describe SocialSnippet::Resolvers::InsertResolver, :current => true do
       end
     end
 
-    let(:resolver) { ::SocialSnippet::Resolvers::InsertResolver.new(fake_social_snippet) }
-
     context "no options" do
+
+      let(:resolver) do
+        ::SocialSnippet::Resolvers::InsertResolver.new(fake_social_snippet)
+      end
 
       let(:input) do
         [
@@ -39,7 +41,89 @@ describe SocialSnippet::Resolvers::InsertResolver, :current => true do
       subject { resolver.insert input }
       it { should eq expected }
 
-    end # create instance
+    end # no options
+
+    context ":margin_top => 3" do
+
+      let(:resolver) do
+        ::SocialSnippet::Resolvers::InsertResolver.new(fake_social_snippet, {
+          :margin_top => 3,
+        })
+      end
+
+      let(:input) do
+        [
+          "// @snip <my-repo-1:path/to/file.d>",
+          "// @snip <my-repo-2:path/to/file.d>",
+          "// @snip <my-repo-3:path/to/file.d>",
+        ].join($/)
+      end
+
+      let(:expected) do
+        [
+          "",
+          "",
+          "",
+          "// @snippet <my-repo-1:path/to/file.d>",
+          "my-repo-1",
+          "",
+          "",
+          "",
+          "// @snippet <my-repo-2:path/to/file.d>",
+          "my-repo-2",
+          "",
+          "",
+          "",
+          "// @snippet <my-repo-3:path/to/file.d>",
+          "my-repo-3",
+        ].join($/)
+      end
+
+      subject { resolver.insert input }
+      it { should eq expected }
+
+    end # :margin_top
+
+    context ":margin_bottom => 3" do
+
+      let(:resolver) do
+        ::SocialSnippet::Resolvers::InsertResolver.new(fake_social_snippet, {
+          :margin_bottom => 3,
+        })
+      end
+
+      let(:input) do
+        [
+          "// @snip <my-repo-1:path/to/file.d>",
+          "// @snip <my-repo-2:path/to/file.d>",
+          "// @snip <my-repo-3:path/to/file.d>",
+        ].join($/)
+      end
+
+      let(:expected) do
+        [
+          "// @snippet <my-repo-1:path/to/file.d>",
+          "my-repo-1",
+          "",
+          "",
+          "",
+          "// @snippet <my-repo-2:path/to/file.d>",
+          "my-repo-2",
+          "",
+          "",
+          "",
+          "// @snippet <my-repo-3:path/to/file.d>",
+          "my-repo-3",
+          "",
+          "",
+          "",
+        ].join($/)
+      end
+
+      subject { resolver.insert input }
+      it { should eq expected }
+
+    end # :margin_bottom
 
   end # prepare stubs
 
