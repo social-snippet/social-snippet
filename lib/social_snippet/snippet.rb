7a52a9e654a557312abbc88c515398af1f75b28b
Hiroyuki Sano <sh19910711@gmail.com>
2015-02-13 11:29:41 +0900

    snippet: add Snippet.new_text()

--- lib/social_snippet/snippet.rb
+++ lib/social_snippet/snippet.rb
@@ -1,14 +1,64 @@
 class SocialSnippet::Snippet
 
-  attr_reader :path
+  attr_reader :filepath
   attr_reader :code
-  attr_reader :lines
 
   # Constructor
   def initialize(snippet_path)
-    @path = snippet_path
-    @code = ::File.read(path)
-    @lines = code.split($/)
+    @filepath = snippet_path
+    read_file unless filepath.nil?
+  end
+
+  def read_file
+    @code = ::File.read(filepath)
+  end
+
+  def read_text(s)
+    @code = s
+  end
+
+  def lines
+    @lines ||= new_lines
+  end
+
+  class << self
+
+    # Create instance by text
+    def new_text(s)
+      snippet = self.new(nil)
+      snippet.read_text s
+      snippet
+    end
+
+  end
+
+  private
+
+  # Return filtered and styled lines
+  def new_lines
+    lines = code.split $/
+    lines
+  end
+
+  # @param lines [Array<String>]
+  def filter(lines)
+    lines = cut_filter(lines)
+    lines
+  end
+
+  def cut_filter(lines)
+    cut_level = 0
+    lines.select do |line|
+      if Tag.is_begin_cut?(line)
+        cut_level += 1
+        false
+      elsif Tag.is_end_cut?(line)
+        cut_level -= 1
+        false
+      else
+        cut_level === 0
+      end
+    end
   end
 
 end
