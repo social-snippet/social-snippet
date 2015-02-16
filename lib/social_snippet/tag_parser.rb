2593d1844848767215ffb0218ecabad889701549
Hiroyuki Sano <sh19910711@gmail.com>
2015-02-16 16:58:38 +0900

    tag_parser: add find_no_tags()

--- lib/social_snippet/tag_parser.rb
+++ lib/social_snippet/tag_parser.rb
@@ -1,51 +1,60 @@
-class SocialSnippet::TagParser
+module SocialSnippet
 
-  class << self
+  class TagParser
 
-    # Find `@snip` tags from text
-    #
-    # @param s [String or Array] parsed text
-    # @return [Array] found `@snip` tags with line_no
-    def find_snip_tags(s)
-      find_lines(s) {|line| ::SocialSnippet::Tag.is_snip_tag_line(line) }
-    end
+    class << self
 
-    # Find `@snippet` tags from text
-    #
-    # @param s [String or Array] parsed text
-    # @return [Array] found `@snippet` tags with line_no
-    def find_snippet_tags(s)
-      find_lines(s) {|line| ::SocialSnippet::Tag.is_snippet_tag_line(line) }
-    end
+      # Find `@snip` tags from text
+      #
+      # @param s [String or Array] parsed text
+      # @return [Array] found `@snip` tags with line_no
+      def find_snip_tags(s)
+        find_lines(s) {|line| Tag.is_snip_tag_line(line) }
+      end
 
-    private
+      # Find `@snippet` tags from text
+      #
+      # @param s [String or Array] parsed text
+      # @return [Array] found `@snippet` tags with line_no
+      def find_snippet_tags(s)
+        find_lines(s) {|line| Tag.is_snippet_tag_line(line) }
+      end
 
-    def find_lines(s, &comparator)
-      get_lines(s).each.with_index.inject([]) do |found_lines, (line, i)|
-        if comparator.call(line)
-          found_lines.push(
-            {
-              :line_no => i,
-              :tag => ::SocialSnippet::Tag.new(line),
-            }
-          )
+      def find_no_tags(s)
+        find_lines(s) {|line| Tag.is_no_tag_line?(line) }
+      end
+
+      private
+
+      def find_lines(s, &comparator)
+        get_lines(s).each.with_index.inject([]) do |found_lines, (line, i)|
+          if comparator.call(line)
+            found_lines.push(
+              {
+                :line_no => i,
+                :tag => Tag.new(line),
+              }
+            )
+          end
+          found_lines
         end
-        found_lines
       end
-    end
 
-    def get_lines(s)
-      if s.is_a?(String)
-        s.split($/)
-      elsif s.is_a?(Array)
-        s
-      elsif s.is_a?(Enumerator)
-        s
-      else
-        raise "error unknown data"
+      def get_lines(s)
+        if s.is_a?(::String)
+          s.split($/)
+        elsif s.is_a?(::Array)
+          s
+        elsif s.is_a?(::Enumerator)
+          s
+        else
+          raise "error unknown data"
+        end
       end
+
     end
 
   end
 
 end
+
