ce0ea0ecc92497cebb2467625667bb9d6fdf6aae
Hiroyuki Sano <sh19910711@gmail.com>
2015-02-25 12:42:12 +0900

    completion_api: remove cli_complete_snippet_path()

--- lib/social_snippet/api/completion_api.rb
+++ lib/social_snippet/api/completion_api.rb
@@ -1,12 +1,8 @@
 module SocialSnippet::Api::CompletionApi
 
   def complete_snippet_path(keyword)
-    core.repo_manager.complete(keyword)
-  end
-
-  def cli_complete_snippet_path(keyword)
-    complete_snippet_path(keyword).each do |cand_repo|
-      output cand_repo
+    core.repo_manager.complete(keyword).each do |snippet_path|
+      output snippet_path
     end
   end
 
