40d2e06cd6c668d0642865db18a7d0f7ec5123ac
Hiroyuki Sano <sh19910711@gmail.com>
2015-03-04 14:26:02 +0900

    storage_backend: fix directory name

--- lib/social_snippet/storage_backend.rb
+++ lib/social_snippet/storage_backend.rb
@@ -1,3 +1,3 @@
-require_relative "storage/storage_base"
-require_relative "storage/file_system_storage"
+require_relative "storage_backend/storage_base"
+require_relative "storage_backend/file_system_storage"
 module SocialSnippet::StorageBackend; end
