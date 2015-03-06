636c4a474e8ba881d7235f9e480dce0322214b14
Hiroyuki Sano <sh19910711@gmail.com>
2015-03-06 11:36:07 +0900

    yaml_document: implemente methods

--- lib/social_snippet/document_backend/yaml_document.rb
+++ lib/social_snippet/document_backend/yaml_document.rb
@@ -1,37 +1,155 @@
 module SocialSnippet::DocumentBackend
 
+  $yaml_document_hash = ::Hash.new
+
   class YAMLDocument
 
-    def self.yaml_path(new_path)
-      @path = new_path
+    attr_reader :path
+    attr_reader :field_keys
+    attr_reader :fields
+    attr_reader :collection
+
+    attr_accessor :id
+
+    def initialize(options = {}, new_id = nil)
+      @path   = self.class.path
+      @collection = self.class.collection
+      @fields = ::Hash.new
+      @field_keys = self.class.field_keys
+      init_fields options
+      @id ||= new_id
     end
 
-    def self.field(sym, options = {})
-      fields ||= {}
+    def serialize
+      attrs = field_keys.inject(::Hash.new) do |attrs, key|
+        attrs[key] = fields[key]
+        attrs
+      end
+      attrs[:id] = id
+      attrs
+    end
 
-      # set default value
-      fields[sym] = options[:default] unless options[:default].nil?
+    def init_fields(options = {})
+      field_keys.each do |k|
+        fields[k] = self.class.default_field[k]
+      end
+      options.each do |k, v|
+        fields[k] = v
+      end
+    end
 
-      # define getter
-      define_method sym.to_s do
-        fields[sym]
+    def remove
+      collection.delete id
+      self.class.update_file!
+    end
+
+    def save!
+      collection[id] = serialize
+      self.class.update_file!
+    end
+
+    class YAMLDocumentQuery
+
+      attr_reader :collection
+
+      def initialize(new_collection)
+        @collection = new_collection
       end
 
-      # define setter
-      define_method "#{sym.to_s}=" do |v|
-        fields[sym] = v
+      def exists?
+        collection.size > 0
       end
+
+      def find(cond)
+        collection.select do |item_id, item|
+          cond.any? {|k, _| cond[k] === item[k] }
+        end
+        self.class.new collection
+      end
+
     end
 
-    def self.create(options = {})
-    end
+    class << self
 
-    def self.activate!
-      ::SocialSnippet.class_eval do
-        remove_const :Document
-        const_set :Document, YAMLDocument
+      def set_path(new_path)
+        @path = new_path
+        ::FileUtils.touch(path) unless ::File.exists?(path)
       end
-    end
+
+      def path
+        if self != ::SocialSnippet::DocumentBackend::YAMLDocument
+          ::SocialSnippet::DocumentBackend::YAMLDocument.path
+        else
+          @path
+        end
+      end
+
+      def update_file!
+        ::File.write path, $yaml_document_hash.to_yaml
+      end
+
+      def find(id)
+        if collection.has_key?(id)
+          new collection[id], id
+        else
+          raise "ERROR: not found document"
+        end
+      end
+
+      def where(cond)
+        YAMLDocumentQuery.new(collection).find cond
+      end
+
+      def collection
+        collection ||= create_collection
+      end
+
+      def create_collection
+        $yaml_document_hash[self.to_s.downcase] ||= ::Hash.new
+      end
+
+      def field_keys
+        @field_keys
+      end
+
+      def default_field
+        @default_field
+      end
+
+      def field(sym, options = {})
+        @field_keys ||= ::Set.new
+        @default_field ||= ::Hash.new
+
+        default_field[sym] = options[:default] unless options[:default].nil?
+
+        field_keys.add sym
+
+        # define getter
+        define_method sym do
+          @fields[sym]
+        end
+
+        # define setter
+        define_method "#{sym}=" do |v|
+          @fields[sym] = v
+        end
+      end
+
+      def create(options = {})
+        doc = new
+        options.each {|k, v| doc.send "#{k}=", v }
+        doc
+      end
+
+      # replace self with ::SocialSnippet::Document class
+      def activate!
+        ::SocialSnippet.class_eval do
+          remove_const :Document
+          const_set :Document, YAMLDocument
+        end
+      end
+
+    end # class << self
 
   end
 
