285ecb2e714bfa33295b76fcb0e544bae2e01e2a
Hiroyuki Sano <sh19910711@gmail.com>
2015-03-09 11:03:27 +0900

    test: add tests for YAMLDocument

--- test/yaml_document_test.rb
+++ test/yaml_document_test.rb
@@ -12,12 +12,28 @@ describe ::SocialSnippet::DocumentBackend::YAMLDocument, :current => true do
 
       before do
         ::File.write "/path/to/document.yml", {
-          "testdocument" => {
+          "TestDocument" => {
             "item1" => {
               "name" => "item-1",
+              "values" => [
+                "val1-1",
+                "val1-2",
+                "val1-3",
+              ],
+              "info" => {
+                "hello" => "world",
+              },
             },
             "item2" => {
               "name" => "item-2",
+              "values" => [
+                "val1-1",
+                "val1-2",
+                "val1-3",
+              ],
+              "info" => {
+                "foo" => "bar",
+              },
             },
           },
         }.to_yaml
@@ -25,17 +41,51 @@ describe ::SocialSnippet::DocumentBackend::YAMLDocument, :current => true do
 
       context "prepare class" do
 
-        let(:klazz) do
-          class TestDocument < ::SocialSnippet::DocumentBackend::YAMLDocument
+        let(:doc_class) do
+          ::Class.new(::SocialSnippet::DocumentBackend::YAMLDocument) do
             field :name, :type => String
+            field :values, :type => Array
+            field :info, :type => Hash
+
+            def self.name
+              "TestDocument"
+            end
           end
-          TestDocument
         end
 
         context "find item1" do
-          let(:item) { klazz.find "item1" }
+          let(:item) { doc_class.find "item1" }
           it { expect(item.name).to eq "item1" }
+          it { expect(item.values.length).to 3 }
+          it { expect(item.values).to include "val1-1" }
+          it { expect(item.values).to include "val1-2" }
+          it { expect(item.values).to include "val1-3" }
+          it { expect(item.info["hello"]).to eq "world" }
         end
+
+        context "find item2" do
+          let(:item) { doc_class.find "item2" }
+          it { expect(item.name).to eq "item2" }
+          it { expect(item.values.length).to 3 }
+          it { expect(item.values).to include "val2-1" }
+          it { expect(item.values).to include "val2-2" }
+          it { expect(item.values).to include "val2-3" }
+          it { expect(item.info["foo"]).to eq "bar" }
+        end
+
+        context "remove item2" do
+          before { doc_class.find("item2").remove }
+          context "find item2" do
+            subject do
+              lambda { doc_class.find "item2" }
+            end
+            it { should raise_error }
+          end
+          context "find item1" do
+            let(:item) { doc_class.find("item1") }
+            it { expect(item.name).to eq "item1" }
+          end
+        end # remove item2
 
       end
 
