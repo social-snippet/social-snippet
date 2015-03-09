d35053965483986385e50eb9fe6a2c9bfccdb3c4
Hiroyuki Sano <sh19910711@gmail.com>
2015-03-09 17:50:55 +0900

    test: fix tests

--- spec/lib/yaml_document_spec.rb
+++ spec/lib/yaml_document_spec.rb
@@ -2,24 +2,11 @@ require "spec_helper"
 
 describe ::SocialSnippet::DocumentBackend::YAMLDocument do
 
-  context "set yaml path" do
+  context "activate" do
 
-    let(:document_path) { "/path/to/document.yml" }
+    before { ::SocialSnippet::DocumentBackend::YAMLDocument.activate! }
 
-    before do
-      ::FileUtils.mkdir_p "/path/to"
-      $yaml_document_hash = nil
-      ::FileUtils.rm document_path if ::File.exists?(document_path)
-      ::SocialSnippet::DocumentBackend::YAMLDocument.set_path document_path
-    end
-
-    context "activate" do
-
-      before { ::SocialSnippet::DocumentBackend::YAMLDocument.activate! }
-
-      include_context :TestDocument
-
-    end
+    include_context :TestDocument
 
   end
 
