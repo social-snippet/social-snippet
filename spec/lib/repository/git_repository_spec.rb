affb8600e4f008df8f70a4d0f15733158ec5f222
Hiroyuki Sano <sh19910711@gmail.com>
2015-03-11 20:45:11 +0900

    git_repository_spec: fix tests

--- spec/lib/repository/git_repository_spec.rb
+++ spec/lib/repository/git_repository_spec.rb
@@ -4,18 +4,12 @@ module SocialSnippet::Repository::Drivers
 
   describe GitDriver, :without_fakefs => true do
 
-    before { disable_fakefs }
-
     context "github repo" do
 
       context "clone social-snippet/example-repo" do
 
         subject(:repo) { fake_core.repo_factory.clone("git://github.com/social-snippet/example-repo.git") }
-
-        context "load snippet.json" do
-          before { repo.load_snippet_json }
-          it { expect(repo.name).to eq "example-repo" }
-        end
+        it { expect(repo.name).to eq "example-repo" }
 
         describe "#refs" do
           subject { repo.refs }
@@ -31,47 +25,10 @@ module SocialSnippet::Repository::Drivers
           it { should include "1.0.1" }
         end
 
-        context "checkout 1.0.0" do
-
-          before do
-            repo.checkout("1.0.0")
-            repo.load_snippet_json
-          end
-
-          it { expect(repo.name).to eq "example-repo" }
-
-          describe "#commit_id" do
-            subject { repo.commit_id }
-            it { should eq "efa58ecae07cf3d063ae75fa97fce164c56d205a" }
-          end
-
-          describe "#short_commit_id" do
-            subject { repo.short_commit_id }
-            it { should eq "efa58eca" }
-          end
-
-        end # checkout 1.0.0
-
-        context "checkout 1.0.x (latest)" do
-
-          before do
-            repo.checkout(repo.latest_version("1.0"))
-            repo.load_snippet_json
-          end
-
-          it { expect(repo.name).to eq "example-repo" }
-
-          describe "#commit_id" do
-            subject { repo.commit_id }
-            it { should eq "38ebf733622174e24d76d28ab264c52b4fd7bda9" }
-          end
-
-          describe "#short_commit_id" do
-            subject { repo.short_commit_id }
-            it { should eq "38ebf733" }
-          end
-
-        end # checkout 1.0.x
+        describe "#latest_version" do
+          subject { repo.latest_version("1.0") }
+          it { should eq "1.0.2" }
+        end
 
       end # clone social_snippet/example-repo
 
