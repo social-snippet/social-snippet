75216a570f136e4dbe7f8f3d91b41a5377c43539
Hiroyuki Sano <sh19910711@gmail.com>
2015-03-11 10:43:19 +0900

    models: add Repository model

--- lib/social_snippet/repository/models.rb
+++ lib/social_snippet/repository/models.rb
@@ -1,9 +1,12 @@
 module SocialSnippet::Repository::Models
 
   def self.const_missing(name)
-    if name == :Package
+    if name === :Package
       require_relative "models/package"
       ::SocialSnippet::Repository::Models::Package
+    elsif name === :Repository
+      require_relative "models/repository"
+      ::SocialSnippet::Repository::Models::Repository
     else
       super
     end
