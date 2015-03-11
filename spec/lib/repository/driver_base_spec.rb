5b3b69be47aaeed830f7a94f9de736ccfb332529
Hiroyuki Sano <sh19910711@gmail.com>
2015-03-11 13:23:28 +0900

    driver_base_spec: add tests

--- spec/lib/repository/driver_base_spec.rb
+++ spec/lib/repository/driver_base_spec.rb
@@ -3,6 +3,7 @@ require "spec_helper"
 describe ::SocialSnippet::Repository::Drivers::DriverBase, :current => true do
 
   let(:repo_url) { "git://github.com/user/repo" }
+  let(:repo_name) { "my-repo" }
   let(:driver) do
     ::SocialSnippet::Repository::Drivers::DriverBase.new fake_core, repo_url
   end
@@ -14,7 +15,7 @@ describe ::SocialSnippet::Repository::Drivers::DriverBase, :current => true do
   before do
     allow(driver).to receive(:snippet_json) do
       {
-        "name" => "repo",
+        "name" => "my-repo",
       }
     end
   end
@@ -23,6 +24,10 @@ describe ::SocialSnippet::Repository::Drivers::DriverBase, :current => true do
     allow(driver).to receive(:current_ref) do
       "master"
     end
+  end
+
+  before do
+    allow(driver).to receive(:latest_version).and_return "0.0.2"
   end
 
   before do
@@ -35,11 +40,11 @@ describe ::SocialSnippet::Repository::Drivers::DriverBase, :current => true do
       when "feature/abc"
         "rev-feature-abc"
       when "0.0.0"
-        "0.0.0"
+        "rev-0.0.0"
       when "0.0.1"
-        "0.0.1"
+        "rev-0.0.1"
       when "0.0.2"
-        "0.0.2"
+        "rev-0.0.2"
       else
         raise "error"
       end
@@ -50,7 +55,7 @@ describe ::SocialSnippet::Repository::Drivers::DriverBase, :current => true do
     allow(driver).to(
       receive(:each_directory)
         .and_yield(::SocialSnippet::Repository::Drivers::Entry.new "dir1")
-        .and_yield(::SocialSnippet::Repository::Drivers::Entry.new "dir2/subdir1")
+        .and_yield(::SocialSnippet::Repository::Drivers::Entry.new "dir1/subdir1")
         .and_yield(::SocialSnippet::Repository::Drivers::Entry.new "dir1/subdir2")
         .and_yield(::SocialSnippet::Repository::Drivers::Entry.new "dir1/subdir3")
         .and_yield(::SocialSnippet::Repository::Drivers::Entry.new "dir2")
@@ -63,7 +68,7 @@ describe ::SocialSnippet::Repository::Drivers::DriverBase, :current => true do
       receive(:each_content)
         .and_yield(::SocialSnippet::Repository::Drivers::Entry.new "file1", "")
         .and_yield(::SocialSnippet::Repository::Drivers::Entry.new "dir1/file2", "")
-        .and_yield(::SocialSnippet::Repository::Drivers::Entry.new "dir2/subdir1/file3", "")
+        .and_yield(::SocialSnippet::Repository::Drivers::Entry.new "dir1/subdir1/file3", "")
         .and_yield(::SocialSnippet::Repository::Drivers::Entry.new "dir1/subdir2/file4", "")
         .and_yield(::SocialSnippet::Repository::Drivers::Entry.new "dir1/subdir3/file5", "")
         .and_yield(::SocialSnippet::Repository::Drivers::Entry.new "dir2/file6", "")
@@ -91,7 +96,7 @@ describe ::SocialSnippet::Repository::Drivers::DriverBase, :current => true do
 
       before { driver.cache }
 
-      context "::SocialSnippet::Repository::Models::Repository.find repo" do
+      context "::SocialSnippet::Repository::Models::Repository.find repo_url" do
 
         let(:repo) do
           ::SocialSnippet::Repository::Models::Repository.find_by(:url => repo_url)
@@ -99,7 +104,47 @@ describe ::SocialSnippet::Repository::Drivers::DriverBase, :current => true do
 
         context "repo.name" do
           subject { repo.name }
-          it { should eq "repo" }
+          it { should eq repo_name }
+        end
+
+        context "repo.has_versions?" do
+          subject { repo.has_versions? }
+          it { should be_truthy }
+        end
+
+        context "repo.latest_version" do
+          subject { repo.latest_version }
+          it { should eq "0.0.2" }
+        end
+
+      end
+
+      context "find package 0.0.2" do
+
+        let(:package) do
+          ::SocialSnippet::Repository::Models::Package.find_by(
+            :repo_name => repo_name,
+            :rev_hash => "rev-0.0.2",
+          )
+        end
+
+        context "package.paths" do
+          subject { package.paths }
+          it { should_not be_empty }
+          it { expect(subject).to include "file1" }
+          it { expect(subject).to include "dir1/file2" }
+          it { expect(subject).to include "dir1/subdir1/file3" }
+          it { expect(subject).to include "dir1/subdir2/file4" }
+          it { expect(subject).to include "dir1/subdir3/file5" }
+          it { expect(subject).to include "dir2/file6" }
+          it { expect(subject).to include "dir3/file7" }
+        end
+
+        context "load snippet.json" do
+          subject do
+            lambda { package.load_snippet_json }
+          end
+          it { should raise_error ::Errno::ENOENT }
         end
 
       end
