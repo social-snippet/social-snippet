f9eefddc77283082326df4cf9c0a6958a532310a
Hiroyuki Sano <sh19910711@gmail.com>
2015-03-13 13:01:50 +0900

    sspm_install_spec: add package dependencies test

--- spec/lib/command_line/sspm_install_spec.rb
+++ spec/lib/command_line/sspm_install_spec.rb
@@ -1,6 +1,6 @@
 require "spec_helper"
 
-describe ::SocialSnippet::CommandLine::SSpm::SubCommands::InstallCommand, :current => true do
+describe ::SocialSnippet::CommandLine::SSpm::SubCommands::InstallCommand do
 
   before do
     allow_any_instance_of(
@@ -10,41 +10,12 @@ describe ::SocialSnippet::CommandLine::SSpm::SubCommands::InstallCommand, :curre
     end
   end
 
-  before do
-    my_repo_info = {
-      "name" => "my-repo",
-      "desc" => "This is new repository.",
-      "url" => "git://driver.test/user/my-repo",
-      "dependencies" => {
-      },
-    }
-
-    WebMock
-      .stub_request(
-        :get,
-        "http://api.server/api/dummy/repositories/my-repo",
-      )
-      .to_return(
-        :status => 200,
-        :body => my_repo_info.to_json,
-        :headers => {
-          "Content-Type" => "application/json",
-        },
-      )
-  end # GET /repositories/my-repo
-
-  before do
-    allow(fake_core.api).to receive(:install_repository).and_return false
-    allow(fake_core.api).to receive(:resolve_name_by_registry).and_return false
-  end
-
-  before do
-    allow(fake_core.api).to receive(:resolve_name_by_registry).with("my-repo") do
-      "git://driver.test/user/my-repo"
-    end
-  end
-
   describe "$ sspm install" do
+
+    before do
+      allow(fake_core.api).to receive(:install_repository).and_return false
+      allow(fake_core.api).to receive(:resolve_name_by_registry).and_return false
+    end
 
     context "create snippet.json" do
 
@@ -83,14 +54,61 @@ describe ::SocialSnippet::CommandLine::SSpm::SubCommands::InstallCommand, :curre
 
   describe "$ sspm install my-repo" do
 
-    let(:install_command) { ::SocialSnippet::CommandLine::SSpm::SubCommands::InstallCommand.new ["my-repo"] }
-    it { expect(fake_core.api).to receive(:install_repository).with("git://driver.test/user/my-repo", nil, kind_of(::Hash)) }
-    after do
-      install_command.init
-      install_command.run
-    end
+    context "prepare registry info" do
+
+      before do
+        allow(fake_core.api).to receive(:resolve_name_by_registry).with("my-repo") do
+          "git://driver.test/user/my-repo"
+        end
+      end
+
+      context "install my-repo" do
+        let(:install_command) { ::SocialSnippet::CommandLine::SSpm::SubCommands::InstallCommand.new ["my-repo"] }
+        it { expect(fake_core.api).to receive(:install_repository).with("git://driver.test/user/my-repo", nil, kind_of(::Hash)) }
+        after do
+          install_command.init
+          install_command.run
+        end
+      end
+
+    end # prepare registry info
 
   end # $ sspm install my-repo
+
+  describe "package dependencies" do
+
+    context "prepare registry info" do
+
+      before do
+        allow(fake_core.api).to receive(:resolve_name_by_registry).with("my-repo") do
+          "git://driver.test/user/my-repo"
+        end
+        allow(fake_core.api).to receive(:resolve_name_by_registry).with("new-repo") do
+          "git://driver.test/user/new-repo"
+        end
+      end
+
+      context "install new-repo" do
+        let(:install_command) { ::SocialSnippet::CommandLine::SSpm::SubCommands::InstallCommand.new ["new-repo"] }
+        example do
+          expect(fake_core.repo_manager).to receive(:install).with("git://driver.test/user/my-repo", "1.2.3", kind_of(::Hash)).once do
+            ::SocialSnippet::Repository::Models::Package.new
+          end
+          expect(fake_core.repo_manager).to receive(:install).with("git://driver.test/user/new-repo", nil, kind_of(::Hash)).once do
+            pkg = ::SocialSnippet::Repository::Models::Package.new
+            pkg.add_dependency "my-repo", "1.2.3"
+            pkg
+          end
+        end
+        after do
+          install_command.init
+          install_command.run
+        end
+      end
+
+    end # prepare registry info
+
+  end
 
 end # ::SocialSnippet::CommandLine::SSpm::SubCommands::InstallCommand
 
