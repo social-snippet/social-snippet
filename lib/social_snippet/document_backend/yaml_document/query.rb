e52af86e0f13def6147ca973b54328a1525a7b2c
Hiroyuki Sano <sh19910711@gmail.com>
2015-03-13 13:56:25 +0900

    yaml_document/query: add document_class variable

--- lib/social_snippet/document_backend/yaml_document/query.rb
+++ lib/social_snippet/document_backend/yaml_document/query.rb
@@ -2,9 +2,11 @@ module SocialSnippet::DocumentBackend
 
   class YAMLDocument::Query
 
+    attr_reader :document_class
     attr_reader :collection
 
-    def initialize(new_collection)
+    def initialize(new_document_class, new_collection)
+      @document_class = new_document_class
       @collection = new_collection
     end
 
@@ -16,11 +18,25 @@ module SocialSnippet::DocumentBackend
       new_collection = collection.select do |item_id, item|
         cond.all? {|k, _| cond[k] === item[k] }
       end
-      self.class.new new_collection
+      self.class.new document_class, new_collection
     end
 
     def count
       collection.length
+    end
+
+    def enum
+      collection.map do |_, item|
+        document_class.new item
+      end
+    end
+
+    def each(&block)
+      enum.each &block
+    end
+
+    def map(&block)
+      enum.map &block
     end
 
   end
