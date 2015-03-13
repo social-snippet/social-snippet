429e9bf33b5cd709a8930541f37c0abde9823cf6
Hiroyuki Sano <sh19910711@gmail.com>
2015-03-13 15:13:38 +0900

    git_driver: set driver to factory class

--- spec/lib/repository/git_driver_spec.rb
+++ spec/lib/repository/git_driver_spec.rb
@@ -1,40 +1,40 @@
 require "spec_helper"
 
-module SocialSnippet::Repository::Drivers
+describe ::SocialSnippet::Repository::Drivers::GitDriver, :without_fakefs => true do
 
-  describe GitDriver, :without_fakefs => true do
+  before do
+    fake_core.repo_factory.add_driver ::SocialSnippet::Repository::Drivers::GitDriver
+  end
 
-    context "github repo" do
+  context "github repo" do
 
-      context "clone social-snippet/example-repo" do
+    context "clone social-snippet/example-repo" do
 
-        subject(:driver) { fake_core.repo_factory.clone("git://github.com/social-snippet/example-repo.git") }
-        it { expect(driver.repo.name).to eq "example-repo" }
+      subject(:driver) { fake_core.repo_factory.clone("git://github.com/social-snippet/example-repo.git") }
+      it { expect(driver.repo.name).to eq "example-repo" }
 
-        describe "#refs" do
-          subject { driver.repo.refs }
-          it { should include "master" }
-          it { should include "1.0.0" }
-          it { should include "1.0.1" }
-        end
+      describe "#refs" do
+        subject { driver.repo.refs }
+        it { should include "master" }
+        it { should include "1.0.0" }
+        it { should include "1.0.1" }
+      end
 
-        describe "#versions" do
-          subject { driver.repo.versions }
-          it { should_not include "master" }
-          it { should include "1.0.0" }
-          it { should include "1.0.1" }
-        end
+      describe "#versions" do
+        subject { driver.repo.versions }
+        it { should_not include "master" }
+        it { should include "1.0.0" }
+        it { should include "1.0.1" }
+      end
 
-        describe "#latest_version" do
-          subject { driver.repo.latest_version("1.0") }
-          it { should eq "1.0.2" }
-        end
+      describe "#latest_version" do
+        subject { driver.repo.latest_version("1.0") }
+        it { should eq "1.0.2" }
+      end
 
-      end # clone social_snippet/example-repo
+    end # clone social_snippet/example-repo
 
-    end # github repo
+  end # github repo
 
-  end # GitDriver
-
-end # SocialSnippet::Repository::Drivers
+end # GitDriver
 
