8f45010af5b7614104b619efddba80a2c20231b7
Hiroyuki Sano <sh19910711@gmail.com>
2015-03-16 11:21:10 +0900

    repository: add package field

--- lib/social_snippet/repository/models/repository.rb
+++ lib/social_snippet/repository/models/repository.rb
@@ -6,10 +6,17 @@ module SocialSnippet::Repository::Models
 
     field :url, :type => String
     field :name, :type => String
-    field :current_ref, :type => String
     field :refs, :type => Array, :default => ::Array.new
     # rev_hash[ref] => Commit ID
     field :rev_hash, :type => Hash, :default => ::Hash.new
+    # package_refs[ref] => rev_hash
+    field :package_refs, :type => Hash, :default => ::Hash.new
+
+    def add_package(ref)
+      modifier = ::Hash.new
+      modifier[ref] = rev_hash[ref]
+      push :package_refs => modifier
+    end
 
     def add_ref(ref, rev_hash)
       add_to_set :refs => ref
@@ -22,15 +29,34 @@ module SocialSnippet::Repository::Models
       refs.include? ref
     end
 
+    def package_ref_keys
+      package_refs.map {|k, _| k }
+    end
+
+    def latest_package_version(pattern = "")
+      find_latest_version package_versions, pattern
+    end
+
+    def package_versions
+      select_versions package_ref_keys
+    end
+
     # Returns latest version
     def latest_version(pattern = "")
-      pattern = "" if pattern.nil?
-      matches = versions.select {|ref| ::SocialSnippet::Version.is_matched_version_pattern(pattern, ref)}
-      ::VersionSorter.rsort(matches).first
+      find_latest_version versions, pattern
     end
 
     # Returns all versions
     def versions
+      select_versions refs
+    end
+
+    def find_latest_version(versions, pattern = "")
+      matches = versions.select {|ref| ::SocialSnippet::Version.is_matched_version_pattern(pattern, ref)}
+      ::VersionSorter.rsort(matches).first
+    end
+
+    def select_versions(refs)
       refs.select {|ref| ::SocialSnippet::Version.is_version(ref) }
     end
 
