a7f2b7250a55e1fbe415f4b92b8b65dd6fac720f
Hiroyuki Sano <sh19910711@gmail.com>
2015-03-18 17:41:02 +0900

    repository_manager: fix get_snippet()

--- lib/social_snippet/repository/repository_manager.rb
+++ lib/social_snippet/repository/repository_manager.rb
@@ -16,7 +16,7 @@ module SocialSnippet::Repository
     # @param context [::SocialSnippet::Context] The context of snippet
     # @param tag [::SocialSnippet::Tag] The tag of snippet
     def get_snippet(context, tag)
-      ::SocialSnippet::Snippet.new(resolve_snippet_path(context, tag))
+      ::SocialSnippet::Snippet.new(core, resolve_snippet_path(context, tag))
     end
 
     # Resolve snippet path by context and tag
