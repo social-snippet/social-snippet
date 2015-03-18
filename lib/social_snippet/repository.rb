2cc5797d82489f16ba6ea02a16efa52291b4cdad
Hiroyuki Sano <sh19910711@gmail.com>
2015-03-18 13:17:46 +0900

    repository.rb: fix required modules order

--- lib/social_snippet/repository.rb
+++ lib/social_snippet/repository.rb
@@ -1,6 +1,6 @@
 module SocialSnippet::Repository; end
-require_relative "repository/drivers"
 require_relative "repository/driver_factory"
+require_relative "repository/drivers"
 require_relative "repository/repository_manager"
 require_relative "repository/repository_errors"
 require_relative "repository/models"
