3aa1e4560bf6828dfc76df185a4f858fb4f3ff30
Hiroyuki Sano <sh19910711@gmail.com>
2015-03-30 15:39:06 +0900

    insert_resolver_spec: add tests for snippet.css

--- spec/lib/insert_resolver_spec.rb
+++ spec/lib/insert_resolver_spec.rb
@@ -191,6 +191,55 @@ describe SocialSnippet::Resolvers::InsertResolver do
 
       end # :margin_bottom
 
+      describe "snippet.css", :current => true do
+
+        context "snippet { margin-bottom: 3 }" do
+
+          before do
+            fake_core.storage.write "snippet.css", [
+              "snippet { margin-bottom: 3; }"
+            ].join($/)
+          end
+
+          let(:resolver) do
+            ::SocialSnippet::Resolvers::InsertResolver.new(fake_core, ::Hash.new)
+          end
+
+          let(:input) do
+            [
+              "// @snip <my-repo-1:path/to/file.d>",
+              "// @snip <my-repo-2:path/to/file.d>",
+              "// @snip <my-repo-3:path/to/file.d>",
+            ].join($/)
+          end
+
+          let(:expected) do
+            [
+              "// @snippet <my-repo-1:path/to/file.d>",
+              "my-repo-1",
+              "",
+              "",
+              "",
+              "// @snippet <my-repo-2:path/to/file.d>",
+              "my-repo-2",
+              "",
+              "",
+              "",
+              "// @snippet <my-repo-3:path/to/file.d>",
+              "my-repo-3",
+              "",
+              "",
+              "",
+            ].join($/)
+          end
+
+          subject { resolver.insert input }
+          it { should eq expected }
+
+        end # snippet { margin-bottom: 3 }
+
+      end # snippet.css
+
     end # test styling
 
   end # prepare stubs
